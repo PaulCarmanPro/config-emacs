ee6b672c5ebee112b56782151a8c845f77fa8b1d
me <me@4x4.nimrac>
2023-08-18 19:22:51 -0400

    debugging install

--- INSTALL.sh
+++ INSTALL.sh
@@ -2,5 +2,4 @@
 #shellcheck disable=SC2155 # declare and assign separately
 
 declare dSource="$(dirname -- "${BASH_SOURCE[0]}")"
-declare dName='emacs.d'
-"$dSource/MakeLinks.sh" "$HOME/" "$dSource/$dName" 
+"$dSource/MakeLinks.sh" "$HOME" "$dSource/src"
